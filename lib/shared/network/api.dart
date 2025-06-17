099e4c7c433909ae891c2464497c79e7c2c647e9
AbdulrahmanElshafie <sabdo6177@gmail.com>
2025-06-17 23:12:55 +0300

    prod changes

--- lib/shared/network/api.dart
+++ lib/shared/network/api.dart
@@ -1,8 +1,8 @@
 class API {
   // Base URL for the API
-  static const String baseUrl = 'http://localhost:8000/api'; // TODO: Replace with actual base URL
+  static const String baseUrl = 'http://172.22.253.81:8000/api'; // TODO: Replace with actual base URL
   
-  // Auth Endpoints
+// Auth Endpoints
   static const String login = '/auth/login/';
   static const String register = '/auth/register/';
   static const String logout = '/auth/logout/';
