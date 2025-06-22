a39e01c5a66721292fc3d571f71629f8d404ea5b
AbdulrahmanElshafie <sabdo6177@gmail.com>
2025-06-22 14:05:23 +0300

    prod

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "160872179"
+    serviceWorkerVersion: "2198023911"
   }
 });
